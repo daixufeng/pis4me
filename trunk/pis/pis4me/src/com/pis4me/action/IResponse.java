b931489b167718f7e7d0c2e145185d2416dfa7cd
xufeng.dai@hotmail.com <xufeng.dai@hotmail.com@892915a3-57e7-2684-3037-33ea689919f1>
2012-04-26 13:17:37 +0000

    delete by daixufeng 2012-04-26

--- trunk/pis/pis4me/src/com/pis4me/action/IResponse.java
+++
@@ -1,8 +0,0 @@
-﻿package com.pis4me.action;
-
-public interface IResponse {
-     void setHasError(Boolean hasError);
-     void setMsg(String msg);
-     void setResponseObject(Object responseObject);
-     String GetResponseString();
-}
