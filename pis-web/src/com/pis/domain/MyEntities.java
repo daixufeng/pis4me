d463f39f1e52428e8e30a2a0ed1af0d522c26b0c
daixufeng <xufeng.dai@hotmail.com>
2019-06-14 10:15:04 +0800

    remove pis-web

--- pis-web/src/com/pis/domain/MyEntities.java
+++
@@ -1,8 +0,0 @@
-package com.pis.domain;
-
-public class MyEntities {
-	public class Category{}
-	public class DailyPay{}
-	public class Dictionary{}
-	public class User{}
-}
--- pis-web/src/com/pis/service/CategoryService.java
+++
@@ -1,8 +0,0 @@
-package com.pis.service;
-
-import java.util.List;
-import java.util.Map;
-
-public interface CategoryService extends MyService {
-	public List<Map<String, Object>> getByType(Long dictionaryId);
-}
